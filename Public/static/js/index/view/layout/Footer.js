a404b7c238bfaaedf92d0cd590b81143b2657826
liyq <allenlee2013@163.com>
2017-09-01 18:09:22 +0800

    修改公司信息

--- Public/static/js/index/view/layout/Footer.js
+++ Public/static/js/index/view/layout/Footer.js
@@ -1,13 +1,13 @@
-Ext.define('admin.view.layout.Footer',{
-	extend:'Ext.panel.Panel',
-	initComponent: function(){
-		Ext.apply(this,{
-			region:'south',
-			cls:'footer',
-			height: 25,
-			split: true,
-			html:'Copyright © 2015 momo Tencent. All Rights Reserved'
-		});
-		this.callParent(arguments);
-	}
+Ext.define('admin.view.layout.Footer', {
+    extend: 'Ext.panel.Panel',
+    initComponent: function() {
+        Ext.apply(this, {
+            region: 'south',
+            cls: 'footer',
+            height: 25,
+            split: true,
+            html: 'Copyright © 2017 广州信天游网络科技. All Rights Reserved'
+        });
+        this.callParent(arguments);
+    }
 });
--- Public/static/js/index/view/layout/Header.js
+++ Public/static/js/index/view/layout/Header.js
@@ -1,22 +1,22 @@
-Ext.define('admin.view.layout.Header',{
-	extend:'Ext.panel.Panel',
-	xtype: 'adminheader',
-	initComponent: function(){
-		Ext.apply(this,{
-			region:'north',
-			height:60,
-			split: true,
-			bodyCls:'header',
-			html:'ThinkPHP+ExtJs 演示',
-			bbar: [
-			  {text:'我的面板',iconCls:'Computeroff',action:'wdmb'},'-',
-			  {text:'系统设置',iconCls:'Cog',action:'xtsz'},'-',
-			  {text:'内容管理',iconCls:'Pagewhiteedit',action:'nrgl'},'-',
-			  {text:'用户管理',iconCls:'Group',action:'yhgl'},'->',
-			  {text:'使用帮助',iconCls:'Help',action:'sybz'},'-',
-			  {text:'退出系统',iconCls:'Usergo',action:'tcxt'}
-			]
-		});
-		this.callParent(arguments);
-	}
+Ext.define('admin.view.layout.Header', {
+    extend: 'Ext.panel.Panel',
+    xtype: 'adminheader',
+    initComponent: function() {
+        Ext.apply(this, {
+            region: 'north',
+            height: 60,
+            split: true,
+            bodyCls: 'header',
+            html: '广州信天游后台管理系统',
+            bbar: [
+                { text: '我的面板', iconCls: 'Computeroff', action: 'wdmb' }, '-',
+                { text: '系统设置', iconCls: 'Cog', action: 'xtsz' }, '-',
+                { text: '内容管理', iconCls: 'Pagewhiteedit', action: 'nrgl' }, '-',
+                { text: '用户管理', iconCls: 'Group', action: 'yhgl' }, '->',
+                { text: '使用帮助', iconCls: 'Help', action: 'sybz' }, '-',
+                { text: '退出系统', iconCls: 'Usergo', action: 'tcxt' }
+            ]
+        });
+        this.callParent(arguments);
+    }
 });
